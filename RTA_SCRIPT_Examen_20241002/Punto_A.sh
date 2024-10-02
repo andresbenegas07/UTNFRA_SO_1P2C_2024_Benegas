d37ba31a4550450f355549a950e3a277bbfbe0f5
andresbenegas07 <andrescirobenegas@gmail.com>
2024-10-02 22:47:58 +0000

    Corregido el script del punto A

--- RTA_SCRIPT_Examen_20241002/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_A.sh
@@ -1,1 +1,5 @@
- sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+#!/bin/bash
+echo "Punto A: Estructura simétrica"
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
+echo "Script ejecutado correctamente"
