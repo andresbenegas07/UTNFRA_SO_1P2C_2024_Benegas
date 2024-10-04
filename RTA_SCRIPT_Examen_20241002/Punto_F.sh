6cedac412e379627f487294a19aa9a1724b1746f
andresbenegas07 <andrescirobenegas@gmail.com>
2024-10-04 20:16:33 +0000

    DOCS:ADD: Agrego el punto F pero sin testear

---
+++ RTA_SCRIPT_Examen_20241002/Punto_F.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Creo el archivo avanzado con los datos de IP, usuario, etc:"
+
+echo
+echo "Mi IP Publica es: $(curl -s ifconfig.me)" > ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt
+
+echo "Mi usuario es: $(whoami)" >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt
+
+echo "El Hash de mi usuario es: $(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}') >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt"
+
+cat /RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt
+
+
+git remote get-url origin
