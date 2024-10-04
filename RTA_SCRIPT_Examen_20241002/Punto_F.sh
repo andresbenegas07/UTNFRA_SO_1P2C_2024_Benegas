781fd813e1470aa3d988d0051ab8329aa55be6a5
andresbenegas07 <andrescirobenegas@gmail.com>
2024-10-04 20:19:35 +0000

    FIX: Agrego el punto F con una corrección

--- RTA_SCRIPT_Examen_20241002/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_F.sh
@@ -9,7 +9,7 @@ echo "Mi usuario es: $(whoami)" >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARC
 
 echo "El Hash de mi usuario es: $(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}') >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt"
 
-cat /RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt
+cat Filtro_Avanzado.txt
 
 
 git remote get-url origin
