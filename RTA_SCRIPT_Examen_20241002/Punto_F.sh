af05afbf70a26548b3bf9077ab5e46dc2ee89af8
andresbenegas07 <andrescirobenegas@gmail.com>
2024-10-07 18:47:04 +0000

    Fix: se corrige comillas

--- RTA_SCRIPT_Examen_20241002/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_F.sh
@@ -7,7 +7,7 @@ echo "Mi IP Publica es: $(curl -s ifconfig.me)" > ~/repogit/UTNFRA_SO_1P2C_2024_
 
 echo "Mi usuario es: $(whoami)" >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt
 
-echo "El Hash de mi usuario es: $(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}') >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt"
+echo "El Hash de mi usuario es: $(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" >> ~/repogit/UTNFRA_SO_1P2C_2024_Benegas/RTA_ARCHIVOS_Examen_20241002/Filtro_Avanzado.txt
 
 cat Filtro_Avanzado.txt
 
